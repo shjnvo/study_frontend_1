29711079e2e31edc5df6c6b6f7ef2ba7b3d0bda5
vo duc loi <shjnvo89@gmail.com>
2020-04-09 16:30:45 +0700

    responsive for mobile app

---
+++ static/javascripts/application.js
@@ -0,0 +1,10 @@
+function openNav() {
+  document.getElementById("mysidebar").style.width = "300px";
+  // document.getElementById("main").style.marginLeft = "250px";
+}
+
+/* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
+function closeNav() {
+  document.getElementById("mysidebar").style.width = "0";
+  // document.getElementById("main").style.marginLeft = "0";
+}
